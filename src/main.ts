4bab5653d94387878a0ae70ab44885254664ea9b
RobertoCarlos881 <roberto881carlos881@hotmail.com>
2024-01-08 01:43:08 -0600

    actualizacion cron

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.setGlobalPrefix('api/v1')
+  //app.setGlobalPrefix('api/v1')
 
   app.useGlobalPipes(
     new ValidationPipe({
